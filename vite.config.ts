579e8d7ee91123c1d56e393443f2ca0651b45f4a
Zisanmimi <nurulhasnat63@gmail.com>
2026-03-11 01:01:50 +0600

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,13 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-     base: "./",
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
     },
   },
-  build: {
-    outDir: 'dist',           // ensures Render can find the publish directory
-    chunkSizeWarningLimit: 2000, // optional, suppress chunk size warnings
-  },
-  server: {
-    hmr: false, // safe default for Render, since HMR isn’t used in production
-  },
-});
+})
